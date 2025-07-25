66e197e1f61efe51bb12db119437a305a3bcd9ce
ShreeKatti <149763446+ShreeKatti@users.noreply.github.com>
2025-07-25 12:29:32 +0530

    about healthcheck api and littel part of mongoose

--- src/app.js
+++ src/app.js
@@ -10,6 +10,14 @@ app.use(
 })
 )
 
+//import routes
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+
+
+//routes
+
+app.use("/api/v1/healthcheck",healthcheckRouter)
+
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
