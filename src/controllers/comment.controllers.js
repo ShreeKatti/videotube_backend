6faa9675338d935ea1a88ca247f56773acab65f7
ShreeKatti <149763446+ShreeKatti@users.noreply.github.com>
2025-08-02 20:36:25 +0530

    looking forward to write backend for comment

---
+++ src/controllers/comment.controllers.js
@@ -0,0 +1,6 @@
+import { Comment } from "../models/comment.models.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+
